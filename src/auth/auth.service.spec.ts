9f26c732f594c17b073da0532141d028ee6e257b
odin <odin@debian.rz>
2022-12-04 20:08:11 -0300

    test(auth): test if jwt is generated

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -45,4 +45,9 @@ describe('AuthService', () => {
 
     expect(validate).toEqual(epectedDataUser);
   });
+  it('should generate jwt token greater then zero', async () => {
+    const login = service.login(mockUser);
+
+    expect(login.jwt).not.toHaveLength(0);
+  });
 });
