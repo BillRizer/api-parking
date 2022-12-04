92018df80984c9072be69427294297dc1d543e73
odin <odin@debian.rz>
2022-12-04 13:55:17 -0300

    chore: add swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { EEnvironments, getEnvironment } from './utils/helpers';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (getEnvironment() == EEnvironments.DEV) {
+    console.log(getEnvironment());
+    //use swagger for dev
+    const config = new DocumentBuilder()
+      .setTitle('Parking API')
+      .setDescription('Parking API documentation')
+      .setVersion('1.0')
+      .addTag('routes')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/v1', app, document);
+  }
+
   await app.listen(3000);
-  console.log('asdasd');
 }
 bootstrap();
