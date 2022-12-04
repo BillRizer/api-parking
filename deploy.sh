3a784aa9d7e183036a35eba1244e04f7601ece2b
odin <odin@debian.rz>
2022-12-04 12:33:07 -0300

    build: add deploy sh and mysql service

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+clear
+
+if [ $1 == "--help" ]; then
+    echo "--clean : clean container before start"
+    echo "--dev : use dev env"
+    echo "--prod : use prod env"
+fi
+
+if [ $1 == "--clean" ]; then
+    echo "If exists, remove..."
+    docker-compose -f "./.docker/docker-compose.yml" --env-file ./.docker/.env.dev --remove-orphans
+    exit 0
+fi
+
+if [ $1 == "--dev" ]; then
+    echo "> starting dev env..."
+    docker-compose -f "./.docker/docker-compose.yml" --env-file ./.docker/.env.dev up --build 
+fi
+
+if [ $1 == "--prod" ]; then
+    echo "> start prod env..."
+    docker-compose -f "./.docker/docker-compose.yml"  --env-file ./.docker/.env.prod up -d --build
+fi
+
+sleep 2
+echo "------------------"
+docker ps -a
+echo "------------------"
