1525da33337f27c43c3219ac9a4dc3ea9122f3d3
odin <odin@debian.rz>
2022-12-04 18:54:52 -0300

    chore: add missing .env and mysql dep

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.TYPE_ENV}`,
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MYSQL_HOST: Joi.string().required(),
+        MYSQL_PORT: Joi.string().required(),
+        MYSQL_USER: Joi.string().required(),
+        MYSQL_PASSWORD: Joi.string().required(),
+        MYSQL_DATABASE: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
