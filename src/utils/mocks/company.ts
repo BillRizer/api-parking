b8582a443eb3e26857ea16b8b7189ac663ef0c60
odin <odin@debian.rz>
2022-12-05 15:31:01 -0300

    temp: add company mock

---
+++ src/utils/mocks/company.ts
@@ -0,0 +1,15 @@
+import { Company } from '../../company/entities/company.entity';
+
+export const mockCompany: Company = {
+  address: '123 Main St',
+  cnpj: '123123123',
+  email: 'test@example.com',
+  id: 'asdaa-asda-sdas-das',
+  max_amount_cars: 10,
+  max_amount_motorcycles: 10,
+  name: 'Test Company',
+  password: 'hashedPassword',
+  phone: '1234567890',
+  createdAt: '',
+  updatedAt: '',
+};
