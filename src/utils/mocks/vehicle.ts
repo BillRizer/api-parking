c01328a23cfff2c3458e252483040ac47a770577
odin <odin@debian.rz>
2022-12-06 14:42:31 -0300

    temp(vehicle): add mock helper to vehicle

---
+++ src/utils/mocks/vehicle.ts
@@ -0,0 +1,13 @@
+import { Vehicle } from '../../vehicle/entities/vehicle.entity';
+
+export const mockVehicle: Vehicle = {
+  id: 'asdaa-asda-sdas-das',
+  brand: 'BMW',
+  color: 'red',
+  model: 'X6 2023',
+  plate: 'API7777',
+  type: 'car',
+  createdAt: '',
+  updatedAt: '',
+  deletedAt: '',
+};
--- src/vehicle/vehicle.module.ts
+++ src/vehicle/vehicle.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { VehicleService } from './vehicle.service';
 import { VehicleController } from './vehicle.controller';
+import { Vehicle } from './entities/vehicle.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Vehicle])],
   controllers: [VehicleController],
   providers: [VehicleService],
+  exports: [VehicleService],
 })
 export class VehicleModule {}
