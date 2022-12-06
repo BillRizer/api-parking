14abe18ee62e54bf813fcc18c0115d8de52243a0
odin <odin@debian.rz>
2022-12-06 14:12:55 -0300

    feat(vehicle): implementation of the controller.create

--- src/vehicle/vehicle.controller.ts
+++ src/vehicle/vehicle.controller.ts
@@ -19,24 +19,4 @@ export class VehicleController {
   create(@Body() createVehicleDto: CreateVehicleDto) {
     return this.vehicleService.create(createVehicleDto);
   }
-
-  @Get()
-  findAll() {
-    return this.vehicleService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.vehicleService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateVehicleDto: UpdateVehicleDto) {
-    return this.vehicleService.update(+id, updateVehicleDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.vehicleService.remove(+id);
-  }
 }
