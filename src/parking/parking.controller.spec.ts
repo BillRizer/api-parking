7471716ba0fd9ffacde630b8a6f6023ff86fbea9
odin <odin@debian.rz>
2022-12-06 22:25:39 -0300

    test(parking): add tests to controller checkIn

--- src/parking/parking.controller.spec.ts
+++ src/parking/parking.controller.spec.ts
@@ -1,9 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { mockCheckIn } from '../utils/mocks/parking';
+import { CheckInParkingDto } from './dto/check-in.dto';
+import { Parking } from './entities/parking.entity';
 import { ParkingController } from './parking.controller';
 import { ParkingService } from './parking.service';
 
+const checkInData = {
+  ...mockCheckIn,
+};
+const checkInEntity = new Parking(checkInData);
+
 describe('ParkingController', () => {
-  let controller: ParkingController;
+  let parkingController: ParkingController;
+  let parkingService: ParkingService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,15 +20,33 @@ describe('ParkingController', () => {
       providers: [
         {
           provide: ParkingService,
-          useValue: {},
+          useValue: {
+            checkIn: jest.fn(),
+          },
         },
       ],
     }).compile();
 
-    controller = module.get<ParkingController>(ParkingController);
+    parkingController = module.get<ParkingController>(ParkingController);
+    parkingService = module.get<ParkingService>(ParkingService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(parkingController).toBeDefined();
+    expect(parkingService).toBeDefined();
+  });
+
+  describe('checkIn', () => {
+    it('should create checkin', async () => {
+      const newCheckIn: CheckInParkingDto = {
+        ...checkInData,
+      };
+      jest
+        .spyOn(parkingService, 'checkIn')
+        .mockResolvedValueOnce(checkInEntity);
+
+      const checkIn = await parkingService.checkIn(newCheckIn);
+      expect(checkIn).toEqual(newCheckIn);
+    });
   });
 });
