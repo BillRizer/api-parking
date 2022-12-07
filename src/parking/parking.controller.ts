281f3fb83f7efaba9a0de56645e72732a3166596
odin <odin@debian.rz>
2022-12-06 22:26:34 -0300

    feature(parking): add feature check-in

--- src/parking/parking.controller.ts
+++ src/parking/parking.controller.ts
@@ -6,15 +6,29 @@ import {
   Patch,
   Param,
   Delete,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { ParkingService } from './parking.service';
 import { CheckInParkingDto } from './dto/check-in.dto';
 import { getCurrentTimeUTC } from '../utils/time';
+import {
+  ApiBadRequestResponse,
+  ApiOkResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { CheckOutParkingDto } from './dto/check-out.dto';
+import * as checkIn from './swagger/checkIn.swagger';
+import * as checkOut from './swagger/checkOut.swagger';
 
 @Controller('parking')
+@ApiTags('parking')
 export class ParkingController {
   constructor(private readonly parkingService: ParkingService) {}
 
+  @ApiOkResponse(checkIn.ApiResponseSuccessNotes)
+  @ApiBadRequestResponse(checkIn.ApiResponseBadRequestNotes)
   @Get('check-in/:id')
   async checkIn(@Param('id') id: string) {
     const checkInParkingDto: CheckInParkingDto = {
@@ -22,6 +36,17 @@ export class ParkingController {
       checkOut: null,
       vehicle: { id: id },
     };
+    console.log(checkInParkingDto);
+
+    const haveCheckInOpen = await this.parkingService.getIfExistCheckinOpen(
+      checkInParkingDto.vehicle.id,
+    );
+    if (haveCheckInOpen) {
+      throw new HttpException(
+        `There is already a check-in open for this vehicle id:${haveCheckInOpen.id}`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
     return await this.parkingService.checkIn(checkInParkingDto);
   }
 
